6f285f193ee35211d0c45da398cc99b7beb92bc0
Showmil <hilyhily334@gmail.com>
2025-12-16 21:17:37 +0900

    fix: main.py 수정

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import uvicorn
 
 def main() -> None:
     """
-    Development entrypoint to run the FastAPI app with autoreload.
+    Development entrypoint to run the FastAPI app with autoreload
     """
     uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
 
